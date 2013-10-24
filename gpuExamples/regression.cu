1cae423a0e3954c5131aba3967710a9d7e790c27
Saher Ahwal <saher@mit.edu>
2013-10-24 08:16:50 -0700

    skeleton regression cuda

--- gpuExamples/regression.cu
+++ gpuExamples/regression.cu
@@ -5,7 +5,20 @@ const int N = 33 * 1024;
 const int threadsPerBlock = 256;
 const int blocksPerGrid = imin (32, (N + threadsPerBlock-1)/ threadsPerBlock);
 
-__global__ void dot( float *a, float *b, float *c){
+
+// struct for storing matrices
+typedef struct {
+    int m;
+    int n;
+    float* elts;
+} Matrix;
+
+
+
+/*
+* kernel to compute dot product between vectors *a and *b. result places in *c.
+*/
+__device__ void dot( float *a, float *b, float *c){
     __shared__ float cache[threadsPerBlock];
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     int cacheIndex = threadIdx.x;
@@ -36,10 +49,34 @@ __global__ void dot( float *a, float *b, float *c){
         c[blockIdx.x] = cache[0];    
 }
 
-__global__ void compute_a_transpose_a( float *a, float *b){
-	
+
+
+/*
+* A: matrix (symmetric and positive definite) 
+* L: L matrix (lower triangular)
+*/
+__device__ void cholesky( Matrix A, Matrix L){
+
 } 
 
+
+/*
+* A,B: matrices to multiply
+* C: resulting matrix of A*B
+*/
+__device__ void matrix_multiply( Matrix A, Matrix B, Matrix C){
+    
+}  
+
+
+/*
+* I_vals: matrix of independent variable values [ [x0,y0,z0...], [x1,y1,z1...], ...]
+* b: vector of values corresponsing to elements in I_vals matrix. 
+* *r: pointer to result of regression - solution
+*/
+__global__ void linear_regression( Matrix I_vals, Matrix b, float *r) {
+    
+}
 
 
 
